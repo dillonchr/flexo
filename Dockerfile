9e4e0b43873a53fe9bbbb1710dbcb46b922edc09
Dillon Christensen <dillon.christensen@kalibrate.com>
2018-08-20 16:13:10 -0500

    forced timezone in container, added super long timeout handler

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM mhart/alpine-node:10.7
+RUN apk add --no-cache tzdata
+ENV TZ America/Chicago
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 WORKDIR /code/
 COPY package*.json ./
 RUN npm i
--- routes/reminders/index.js
+++ routes/reminders/index.js
@@ -1,16 +1,24 @@
 const { extractDate } = require('./reminder-manager.js');
 
+const superSetTimeout = (fn, date) => {
+    const diff = date.getTime() - (new Date().getTime());
+    if (diff > 0x7FFFFFFF) {
+        setTimeout(() => superSetTimeout(date, fn), 0x7FFFFFFF);
+    } else {
+        setTimeout(fn, diff);
+    }
+};
+
 module.exports = (text, from, respondWith) => {
     const date = extractDate(text);
     if (!date) {
         return respondWith('Couldn\'t find the date in the reminder.');
     }
 
-    const delay = date.getTime() - (new Date().getTime());
-    setTimeout(() => {
+    superSetTimeout(() => {
         respondWith(text);
-    }, delay);
-    respondWith(`Will remind you in ${delay}ms or on ${date.toString()}`);
+    }, date);
+    respondWith(`Will remind you on ${date.toString()}`);
 };
 
 module.exports.match = (text) => /^remind/i.test(text);
